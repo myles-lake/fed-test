5ac47edaa020630bf6a7805452805786aaafbe8e
Myles Lake <myles52@live.ca>
2020-02-12 09:29:55 -0500

    Removed string concatination from Mustache template.

--- src/js/main.js
+++ src/js/main.js
@@ -2,20 +2,20 @@ var reportsWidget = {
     options: {
         containerSelector: '.reports',
         template: 
-            '{{#.}}' +
-                '<article id="q4Wrapper" class="reports_item">' +
-                    '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>' +
-                    '</a>' +
-                    '<footer id="q4Wrapper" class="reports_docs">' +
-                        '{{#documents}}' +
-                            '<h3 class="reports_title">' +
-                                '<a href="{{url}}" target="_blank"> {{title}} <span>( {{file_size}} {{file_type}} )</span></a>' +
-                            '</h3>' +
-                        '{{/documents}}' +
-                    '</footer>' +
-                '</article>' +
-            '{{/.}}'
+            `{{#.}}
+                <article id="q4Wrapper" class="reports_item">
+                    <a href="{{cover}}" target="_blank">
+                        <img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>
+                    </a>
+                    <footer id="q4Wrapper" class="reports_docs">
+                        {{#documents}}
+                            <h3 class="reports_title">
+                                <a href="{{url}}" target="_blank"> {{title}} <span>( {{file_size}} {{file_type}} )</span></a>
+                            </h3>
+                        {{/documents}}
+                    </footer>
+                </article>
+            {{/.}}`
     },
 
     init: function () {
